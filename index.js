3cce12701480d909c40bd9553751e0bab3def22b
nise <niels.seidel@nise81.com>
2018-02-21 15:17:18 +0100

    Prepared module export

---
+++ index.js
@@ -0,0 +1,15 @@
+var server = require('./lib/cursor');
+
+
+function viDashboard(stream, options) {
+    return server;
+}
+
+
+viDashboard.server = server;
+
+
+// exports
+// -------
+
+module.exports = viDashboard;
